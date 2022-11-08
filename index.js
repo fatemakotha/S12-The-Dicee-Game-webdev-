1a7935da77547ac8150d2d10230679e97c1f423d
fatemakotha <fatema.alam.kotha@gmail.com>
2022-11-08 19:17:51 +0600

    Create var randomDiceImage

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1; //prints a number between 1 - 6
 
 
-var source = "images\dice"+ randomNumber1 + ".png";
+var randomDiceImage = "dice" + randomNumber1 + ".png"
 
 alert("checking")
