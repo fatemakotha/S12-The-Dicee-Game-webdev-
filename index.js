cd83dab16a10ee81a8beafc62bae477fde8e2b34
fatemakotha <fatema.alam.kotha@gmail.com>
2022-11-08 18:53:16 +0600

    var source = "images\dice"+ randomNumber1 + ".png";

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-var randomNumber1 = Math.floor(Math.random() * 7) //prints a number between 0 - 10
+var randomNumber1 = Math.floor(Math.random() * 7); //prints a number between 0 - 10
+
+
+var source = "images\dice"+ randomNumber1 + ".png";
