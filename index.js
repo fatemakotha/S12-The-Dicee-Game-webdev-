6a4cbbe637f2f72c57f8b3e7f1b2ed6db22bea3c
fatemakotha <fatema.alam.kotha@gmail.com>
2022-11-08 19:16:38 +0600

    Refactored var randomNumber1

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var randomNumber1 = Math.floor(Math.random() * 7); //prints a number between 0 - 10
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; //prints a number between 1 - 6
 
 
 var source = "images\dice"+ randomNumber1 + ".png";
